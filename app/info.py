b8d9732f6352ccb4189778828aee679d847458a6
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-13 17:41:16 +0300

    making the API call the category of news and articles

--- app/info.py
+++ app/info.py
@@ -1,20 +1,32 @@
 from flask import render_template
 from app import app
+from requests import get_news,get_articles
+
 
 # Views
 @app.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
+    general_news = get_news('general')
+	science_news = get_news('science')
+	business_news = get_news('business')
+	technology_news = get_news('technology')
+	health_news = get_news('health')
+	entertainment_news = get_news('entertainment')
+	sports_news = get_news('sports')
+	title = "News Highlight(ニュースハイライト)"
 
-    title = 'Home - Welcome to the best news source outlet'
-    return render_template('index.html', title = title)
+	return render_template('index.html',title = title, general_news = general_news, science_news = science_news, business_news = business_news,technology_news = technology_news, health_news = health_news, entertainment_news = entertainment_news, sports_news = sports_news)
+
     
 @app.route('/news/<int:news_id>')
-def news(news_id):
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('news.html',id = news_id)
+def articles(id):
+	'''
+	view articles page
+	'''
+	articles = get_articles(id)
+	title = f'NH | {id}'
+
+	return render_template('articles.html',title= title,articles = articles))
