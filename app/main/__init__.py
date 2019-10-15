3c360c262c123ee8bdbd5b9801222bc3165fcef9
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-15 11:26:57 +0300

    create blueprint that will define our route

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,19 +1,18 @@
-from flask import Flask
-from flask_bootstrap import Bootstrap 
-from config import config_options
-
-bootstrap = Bootstrap
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from . import info,error
 
 def create_app(config_name):
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-# Setting up configuration
-app.config.from_object(config_options[config_name])
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
-# Initializing Flask Extensions
-bootstrap.init_app(app)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-#Will add the views
-
-return app
+    return app
